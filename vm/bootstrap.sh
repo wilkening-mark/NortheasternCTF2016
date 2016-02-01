a86e30d52fc06ffd95e1135d8934237e7c2372e7
Young-dwarflemur <Thetuxmanguy+github@gmail.com>
2016-02-01 13:32:44 -0500

    Update bootstrap.sh
    added installing python-dev, pip, and twisted, which are necessary to run the server

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -2,7 +2,9 @@
 
 apt-get update
 apt-get install -y socat
-apt-get install python-dev
+apt-get install -y python-dev
+apt-get install -y python-pip
+pip install twisted
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
